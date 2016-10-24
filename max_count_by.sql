01908d3f535f099adc9ec1bc2088bbff16ebb101
Max <maxnis@users.noreply.github.com>
2016-10-24 12:36:14 -0400

    Created

---
+++ max_count_by.sql
@@ -0,0 +1,13 @@
+-- Find customer(s) with max number of orders
+
+-- 1. Create CTE with number of orders per customer
+WITH order_count As (
+	SELECT [CustomerID], COUNT([SalesOrderID]) As OrderCount
+	FROM [AdventureWorks2014].[Sales].[SalesOrderHeader]
+	GROUP BY CustomerID
+)
+
+-- 2. Find customers with no. of orders equal to max
+SELECT CustomerID, OrderCount 
+FROM order_count
+WHERE OrderCount = (SELECT MAX(OrderCount) FROM order_count);
